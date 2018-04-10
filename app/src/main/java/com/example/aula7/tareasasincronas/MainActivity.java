b5b69eeb4c5af7eb68fb30246bc016307566152c
pc722 <pc722@iucesmag.edu.co>
2018-04-10 13:40:29 -0500

    Eventos de MyTask

--- app/src/main/java/com/example/aula7/tareasasincronas/MainActivity.java
+++ app/src/main/java/com/example/aula7/tareasasincronas/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aula7.tareasasincronas;
 
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -41,9 +42,46 @@ public class MainActivity extends AppCompatActivity {
     //Evento de Boton = cargar Datos
     public void loadData(View view){
         if (isOnLine()){
-            Toast.makeText(this, "Todo ... OK", Toast.LENGTH_SHORT).show();
+            MyTask myTask = new MyTask();
+            myTask.execute();
         }else{
             Toast.makeText(this, "No hay conexion a internet", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //**********************************************************************************************
+
+    //Metodo para procesar los datos
+    public void processData(String s){
+        textView.setText("Numero: " + s);
+        textView.setTextSize(Integer.parseInt(s));
+    }
+    //Clase para crear una tarea asincrona
+    public class MyTask extends AsyncTask <String, String, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override//Mostrar lista de 50 Numeros, no se muestra datos a los componentes
+        protected String doInBackground(String... strings) {
+            for (int i = 1; i <= 50;i++){
+                publishProgress(String.valueOf(i));//metodo encargado de pasar de un hilo a uno !=
+            }
+            return "Fin";
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            processData(values[0]);//establezco el valor nuevo en mi textview
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressBar.setVisibility(View.GONE);//Quitar progressBar
         }
     }
 }
